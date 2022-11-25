c7a1672da6ffcdf139dc4e97893f4eb58b079921
Paulo Roberto <61876313+Maximuzsz@users.noreply.github.com>
2022-11-25 15:11:50 -0300

    correção de nomeclaturas

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Documentação com Swagger - Fábrica de Sinapse')
+    .setTitle('')
     .setDescription(
-      'O Swagger (aka OpenApi) é uma biblioteca muito conhecida no universo backend, estando disponível para diversas linguagens e frameworks. Ela gera um site interno no seu backend que descreve, com muitos detalhes, cada endpoint e estrutura de entidades presentes na sua aplicação.',
+      '',
     )
     .setVersion('1.0')
     .build();
